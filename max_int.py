3b0d3bf8e73a44960e899c9021373df058af6ad7
Guðmundur Már <gudmundurk18@ru.is>
2018-09-04 10:29:16 +0000

    temp_num definition moved outside of while loop

--- max_int.py
+++ max_int.py
@@ -1,8 +1,8 @@
+temp_num = 0
 while True:
     num_int = int(input("Input a number: "))    # Do not change this line
     if(num_int < 0):
         break
-    temp_num = 0
     if(temp_num <= num_int):
         temp_num = num_int
     # Fill in the missing code
