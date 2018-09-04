fefe1edbde8cc03e315fb998ab2fcf7cfe9569c5
Guðmundur Már <gudmundurk18@ru.is>
2018-09-04 10:18:08 +0000

    variables temp_num_1 and temp_num_2 defined and given value 0

--- max_int.py
+++ max_int.py
@@ -2,7 +2,8 @@ while True:
     num_int = int(input("Input a number: "))    # Do not change this line
     if(num_int < 0):
         break
-    
+    temp_num_2 = 0
+    temp_num_2 = 0
     # Fill in the missing code
 
     #print("The maximum is", max_int)    # Do not change this line
